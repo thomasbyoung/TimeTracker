6e11be3086cff26d4cbed99cf77468e1cf647824
thomasbyoung <tyoung@kizer.io>
2023-09-05 20:34:46 -0500

    Add project files.

---
+++ TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeEntryController : ControllerBase
+    {
+        private static List<TimeEntry> _timeEntries = new List<TimeEntry>
+        {
+            new TimeEntry
+            {
+                Id = 1,
+                Project = "Time Tracker App",
+                End= DateTime.Now.AddHours(1)
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<TimeEntry>> GetAllTimeEntries()
+        {
+            return Ok(_timeEntries);
+        }
+    }
+}
---
+++ TimeTracker.Shared/Entities/TimeEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Shared.Entities
+{
+    public class TimeEntry
+    {
+        public int Id { get; set; }
+        public required string Project { get; set; }
+        public DateTime Start { get; set; } = DateTime.Now;
+        public DateTime? End { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+    }
+}
